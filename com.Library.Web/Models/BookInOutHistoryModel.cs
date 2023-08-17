8ea58bc3faf35d4c18ff57231808a69f64071958
Binod sapota <binodsapkota96@gmail.com>
2023-08-17 07:36:51 +0545

    book history models

---
+++ com.Library.Web/Models/BookInOutHistoryModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace com.Library.Web.Models
+{
+    public class BookInOutHistoryModel
+    {
+        [Key, Column("ID")]
+        public int Id { get; set; }
+        [Column("BOOK_IDENTIFICATION_ID")]
+
+        public int BookIdentificationId { get; set; }
+        [Column("USER_ID")]
+        public int UserId { get; set; }
+        [Column("ISSUED_TO")]
+        public int IssuedTo { get; set; }
+        [Column("TRAN_DATE")]
+        public int TranDate { get; set; }
+        [Column("BOOK_IN_OUT")]
+        public BookInOut BookInOut { get; set; }
+    }
+
+    public enum BookInOut
+    {
+        Return,
+        Issue,
+        Renew
+        
+    }
+}
--- com.Library.Web/Services/IBookService.cs
+++ com.Library.Web/Services/IBookService.cs
@@ -5,6 +5,7 @@ namespace com.Library.Web.Services
     public interface IBookService : ICommonService<BookModel>
     {
 
-        BookModel AddNewBook(BookModel book, int NoOfBook);
+        List<BookIdentificationModel> AddNewBook(BookModel book, int NoOfBook);
+
     }
 }
